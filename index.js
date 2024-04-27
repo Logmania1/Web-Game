e91faa3605aca3f956929b6d257b667d50635fd9
Logmania1 <158375314+Logmania1@users.noreply.github.com>
2024-04-27 10:54:16 -0400

    added win/lose and modified deck

--- index.js
+++ index.js
@@ -10,16 +10,14 @@ class DeckOfCards {
     init() {
         const suits = ['hearts', 'spades', 'diamonds', 'clubs']
         const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        suits.forEach(suit => {
-            values.forEach(value => {
-                const card = {
-                    suit: suit,
-                    value: value
-                }
-                this.deck.push(card)
-            })
-        })
-    }
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < values.length; j++) {
+                this.deck.push(values[j] + suits[i]);
+            }
+        }
+            }
+        
+    
 
     shuffle() {
         for (let i = this.deck.length - 1; i > 0; i--) {
@@ -30,40 +28,44 @@ class DeckOfCards {
 
     }
 
-    deal() {
-        myDeckOfCards.hand = myDeckOfCards.deck.slice(50)
-        myDeckOfCards.shuffle()
-        myDeckOfCards.dealerHand = myDeckOfCards.deck.slice(50)
+    deal(a) {
+        myDeckOfCards.hand = myDeckOfCards.deck.slice(52-a)
+        
         
     }
-    
-}
-document
-function addImage() {
-    let img = document.createElement('img');
-        img.src = `Assets/CardFaces/${this.value}+${this.suit}.jpg`;
-        document.getElementsByClassName('dealercards').appendChild(img);
+    hit(a) {
+        myDeckOfCards.hand.length = 0
+        myDeckOfCards.hand = myDeckOfCards.deck.slice(52-a)
+        
+    }
+    stay(a) {
+        myDeckOfCards.shuffle()
+        myDeckOfCards.dealerHand = myDeckOfCards.deck.slice(52-a)
+    }
 }
 
+function addImage() {
+    for (let i = 0; i < myDeckOfCards.dealerHand.length; i++) {
+        let img = document.createElement('img');
+        img.src = `Assets/CardFaces/${myDeckOfCards.dealerHand.value}+${myDeckOfCards.dealerHand.suit}.jpg`;
+        document.getElementsByClassName('dealercards').appendChild(img);
+    }
+}
+var dealerSum = 0;
+var yourSum = 0;
+
+var dealerAceCount = 0;
+var playerAceCount = 0;
+
+var hidden;
+var deck;
+
+var canHit = true;
 const myDeckOfCards = new DeckOfCards()
 myDeckOfCards.init()
 myDeckOfCards.shuffle()
 
-const button = document.createElement('button');
-    button.innerText = 'Deal';        
-    button.id = 'deal'; 
-        
-        
-    button.addEventListener('click', () => {    
-        myDeckOfCards.deal();
-        // addImage(myDeckOfCards.dealerHand(0))
-        // addImage(myDeckOfCards.dealerHand(1))
-        console.log(myDeckOfCards.dealerHand)
-        console.log(myDeckOfCards.hand)
-});
-
-document.body.appendChild(button);
 
 // myDeckOfCards.deal()
 
-
+console.log(myDeckOfCards)
